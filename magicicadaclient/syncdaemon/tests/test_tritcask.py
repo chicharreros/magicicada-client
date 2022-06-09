bed54d7faf086d55c76782a262e7af2136e1aa46
Natalia <nataliabidart@gmail.com>
2022-06-09 15:17:01 -0300

    User newer tritcask from master repo and add more abstraction fixes

---
+++ magicicadaclient/syncdaemon/tests/test_tritcask.py
@@ -0,0 +1,152 @@
+# Copyright 2022 Chicharreros (https://launchpad.net/~chicharreros)
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# In addition, as a special exception, the copyright holders give
+# permission to link the code of portions of this program with the
+# OpenSSL library under certain conditions as described in each
+# individual source file, and distribute linked combinations
+# including the two.
+# You must obey the GNU General Public License in all respects
+# for all of the code used other than OpenSSL.  If you modify
+# file(s) with this exception, you may extend this exception to your
+# version of the file(s), but you are not obligated to do so.  If you
+# do not wish to do so, delete this exception statement from your
+# version.  If you delete this exception statement from all source
+# files in the program, then also delete it here.
+"""Tests for Tritcask and helper functions."""
+
+import os
+import uuid
+
+from tritcask import Tritcask as UpstreamTritcask
+
+from magicicadaclient.testing.testcase import BaseTwistedTestCase
+from magicicadaclient.syncdaemon.tritcask import Tritcask
+
+
+class TritcaskTestCase(BaseTwistedTestCase):
+    """Testcase for Tritcask."""
+
+    def make_db(self):
+        base_dir = self.mktemp('data_dir')
+        db = Tritcask(base_dir)
+        self.addCleanup(db.shutdown)
+        return db
+
+    def test_inheritance(self):
+        """Basic test for the put method."""
+        db = self.make_db()
+        self.assertIsInstance(db, UpstreamTritcask)
+
+    def test_put_str(self):
+        """Basic test for the put method when key is a str."""
+        db = self.make_db()
+        key = str(uuid.uuid4())
+        data = os.urandom(50)
+
+        db.put(0, key, data)
+
+        self.assertEqual([(0, key.encode('utf-8'))], list(db._keydir.keys()))
+        self.assertEqual([(0, key)], list(db.keys()))
+        self.assertEqual(db.get(0, key), data)
+
+    def test_put_bytes(self):
+        """Basic test for the put method when key is bytes."""
+        db = self.make_db()
+        key = 'I ♡ unicode'
+        key_bytes = key.encode('utf-8')
+        data = os.urandom(50)
+        db.put(0, key, data)
+
+        self.assertEqual([(0, key_bytes)], list(db._keydir.keys()))
+        self.assertEqual([(0, key)], list(db.keys()))
+        self.assertEqual(db.get(0, key), data)
+
+    def test_get_str(self):
+        """Basic test for the get method when key is a str."""
+        db = self.make_db()
+        key = str(uuid.uuid4())
+        data = os.urandom(50)
+        db.put(0, key, data)
+
+        self.assertEqual([(0, key.encode('utf-8'))], list(db._keydir.keys()))
+        self.assertEqual([(0, key)], list(db.keys()))
+        self.assertEqual(db.get(0, key), data)
+
+    def test_get_bytes(self):
+        """Basic test for the get method when key is bytes."""
+        db = self.make_db()
+        key = 'I ♡ unicode'
+        key_bytes = key.encode('utf-8')
+        data = os.urandom(50)
+        db.put(0, key, data)
+
+        self.assertEqual([(0, key_bytes)], list(db._keydir.keys()))
+        self.assertEqual([(0, key)], list(db.keys()))
+        self.assertEqual(db.get(0, key), data)
+
+    def test_delete_str(self):
+        """Basic test for the get method when key is a str."""
+        db = self.make_db()
+        key = str(uuid.uuid4())
+        data = os.urandom(50)
+        db.put(0, key, data)
+
+        db.delete(0, key)
+
+        self.assertEqual([], list(db._keydir.keys()))
+        self.assertEqual([], list(db.keys()))
+        self.assertRaises(KeyError, db.get, 0, key)
+
+    def test_delete_bytes(self):
+        """Basic test for the get method when key is bytes."""
+        db = self.make_db()
+        key = 'I ♡ unicode'
+        data = os.urandom(50)
+        db.put(0, key, data)
+
+        db.delete(0, key)
+
+        self.assertEqual([], list(db._keydir.keys()))
+        self.assertEqual([], list(db.keys()))
+        self.assertRaises(KeyError, db.get, 0, key)
+
+    def test_keys(self):
+        """Test for the keys() method."""
+        db = self.make_db()
+        keys = []
+        for i in range(3):
+            k = 'I ♡ %s' % i
+            keys.append(k)
+            db.put(0, k, os.urandom(50))
+
+        self.assertEqual([(0, k) for k in keys], list(db.keys()))
+        self.assertEqual(
+            list(db._keydir.keys()), [(0, k.encode('utf-8')) for k in keys])
+
+    def test__contains__(self):
+        """Test for __contains__ method."""
+        db = self.make_db()
+        keys = []
+        for i in range(3):
+            k = 'I ♡ %s' % i
+            keys.append(k)
+            db.put(0, k, os.urandom(50))
+
+        for k in keys:
+            with self.subTest(key=k):
+                self.assertIn((0, k), db)
+                self.assertIn((0, k.encode('utf-8')), db._keydir)
+
+        self.assertNotIn((0, 'I ♡ 3'), db)
